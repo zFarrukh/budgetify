2986ed63928c43a424bc7c4dfc803d778f328ee9
zFarrukh <diplomatfarruh@gmail.com>
2022-03-21 15:46:39 +0500

    add devDependency supertest jest

--- routes/category.router.js
+++ routes/category.router.js
@@ -4,11 +4,9 @@ const isAuth = require('../middlewares/isAuth');
 
 const Router = express.Router();
 
-
-Router.get('/', isAuth, categoryController.getAllcategories);
+Router.get('/', isAuth, categoryController.getAllCategories);
 Router.post('/', isAuth, categoryController.addCategory);
 Router.delete('/:id', isAuth, categoryController.deleteCategoryById);
 Router.put('/:id', isAuth, categoryController.updateCategoryById);
-
 
 module.exports = Router;
