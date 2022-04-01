bf7a0eb65d23d54cfabbcc62e2b696a75e222983
zFarrukh <diplomatfarruh@gmail.com>
2022-04-01 15:08:35 +0500

    add guard to category route

--- client/src/app/categories/categories.module.ts
+++ client/src/app/categories/categories.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'category',
     component: CategoriesPageComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
