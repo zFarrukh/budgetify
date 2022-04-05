685da5c2df2ba8180758f55aca1198314b039c41
zFarrukh <diplomatfarruh@gmail.com>
2022-04-05 10:48:51 +0500

    add route add transaction delete transactionById

--- server/controllers/transaction.controller.js
+++ server/controllers/transaction.controller.js
@@ -1,4 +1,38 @@
 const Transaction = require('../models/transaction.model');
+const Account = require('../models/account.model');
+
+const addTransaction = async (req, res) => {
+  const { title, amount, category, account_id, description, type } = req.body;
+  if ((!title || isNaN(amount) || !category, !account_id || !type)) {
+    return res.status(400).json({ error: 'Bad request' });
+  }
+  if (type !== 'income' && type !== 'expense') {
+    return res.status(400).json({ error: 'Bad request' });
+  }
+  try {
+    const account = await Account.findById(account_id);
+    let newAmount;
+    if (type === 'income') {
+      newAmount = account.amount + amount;
+    } else {
+      newAmount = account.amount - amount;
+    }
+    const transaction = new Transaction({
+      title,
+      amount,
+      category,
+      account_id,
+      description,
+      type,
+      date_of_creation: new Date(),
+    });
+    await transaction.save();
+    await Account.findByIdAndUpdate(account_id, { amount: newAmount });
+    res.json(transaction);
+  } catch (err) {
+    res.status(400).json({ error: 'Bad request' });
+  }
+};
 
 const getTransactions = async (req, res) => {
   const account_id = req.query.account_id;
@@ -13,6 +47,21 @@ const getTransactions = async (req, res) => {
   }
 };
 
+const deleteTransactionById = async (req, res) => {
+  const transaction_id = req.query.id;
+  if (!transaction_id) {
+    return res.status(400).json({ error: 'Bad request' });
+  }
+  try {
+    const transaction = await Transaction.findByIdAndDelete(transaction_id);
+    res.status(200).json(transaction);
+  } catch (err) {
+    res.status(400).json({ error: 'Bad request' });
+  }
+};
+
 module.exports = {
+  addTransaction,
   getTransactions,
+  deleteTransactionById,
 };
--- server/routes/transaction.router.js
+++ server/routes/transaction.router.js
@@ -1,8 +1,13 @@
 const express = require('express');
+
+const isAuth = require('../middlewares/isAuth');
 const transactionController = require('../controllers/transaction.controller');
 
 const Router = express.Router();
 
+Router.use(isAuth);
+
 Router.get('/', transactionController.getTransactions);
+Router.delete('/:id', transactionController.deleteTransactionById);
 
 module.exports = Router;
