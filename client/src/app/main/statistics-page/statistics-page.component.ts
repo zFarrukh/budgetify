7dafeee44bf8997025fe7f80784e1251218adeb1
zFarrukh <diplomatfarruh@gmail.com>
2022-04-18 21:55:47 +0500

    unsubscribe from statistics

--- client/src/app/main/statistics-page/statistics-page.component.ts
+++ client/src/app/main/statistics-page/statistics-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Subscription } from 'rxjs';
 import { AccountService } from '../main-page/account/account.service';
 import { ICategoryStatistics, IMonthlyStatistics } from './statistics.model';
 import { StatisticsService } from './statistics.service';
@@ -11,6 +13,7 @@ import { dateValidator } from './statistics.validator';
   templateUrl: './statistics-page.component.html',
   styleUrls: ['./statistics-page.component.scss'],
 })
+@UntilDestroy({ checkProperties: true })
 export class StatisticsPageComponent implements OnInit {
   monthlyStatistics: IMonthlyStatistics[] = [];
   categoryStatistics: ICategoryStatistics[] = [];
@@ -28,29 +31,33 @@ export class StatisticsPageComponent implements OnInit {
     economy: number;
     economy_percentage: number;
   };
-
+  subscription: Subscription = new Subscription();
   fetchData() {
-    this.statisticsService
-      .getCategoryStatistics(this.account_id, {
-        fromDate: this.range.value.start,
-        toDate: this.range.value.end,
-      })
-      .subscribe((data) => {
-        this.categoryStatistics = data;
-      });
+    this.subscription.add(
+      this.statisticsService
+        .getCategoryStatistics(this.account_id, {
+          fromDate: this.range.value.start,
+          toDate: this.range.value.end,
+        })
+        .subscribe((data) => {
+          this.categoryStatistics = data;
+        })
+    );
 
-    this.statisticsService
-      .getMonthlyStatistics(this.account_id, {
-        fromDate: this.range.value.start,
-        toDate: this.range.value.end,
-      })
-      .subscribe((data) => {
-        this.monthlyStatistics = data;
-        const { total, average } =
-          this.statisticsService.getTotalAndAverageStatistics(data);
-        this.total = total;
-        this.average = average;
-      });
+    this.subscription.add(
+      this.statisticsService
+        .getMonthlyStatistics(this.account_id, {
+          fromDate: this.range.value.start,
+          toDate: this.range.value.end,
+        })
+        .subscribe((data) => {
+          this.monthlyStatistics = data;
+          const { total, average } =
+            this.statisticsService.getTotalAndAverageStatistics(data);
+          this.total = total;
+          this.average = average;
+        })
+    );
   }
 
   range = new FormGroup({
@@ -79,38 +86,44 @@ export class StatisticsPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.accountService.selectAccount.subscribe((account) => {
-      this.account_id = account._id;
-      this.statisticsService
-        .getMonthlyStatistics(this.account_id)
-        .subscribe((statistics) => {
-          this.monthlyStatistics = statistics;
-          const { total, average } =
-            this.statisticsService.getTotalAndAverageStatistics(statistics);
-          this.total = total;
-          this.average = average;
-        });
-      this.statisticsService
-        .getCategoryStatistics(this.account_id)
-        .subscribe((statistics) => {
-          this.categoryStatistics = statistics;
-        });
-    });
+    this.subscription.add(
+      this.accountService.selectAccount.subscribe((account) => {
+        this.account_id = account._id;
+        this.statisticsService
+          .getMonthlyStatistics(this.account_id)
+          .subscribe((statistics) => {
+            this.monthlyStatistics = statistics;
+            const { total, average } =
+              this.statisticsService.getTotalAndAverageStatistics(statistics);
+            this.total = total;
+            this.average = average;
+          });
+        this.statisticsService
+          .getCategoryStatistics(this.account_id)
+          .subscribe((statistics) => {
+            this.categoryStatistics = statistics;
+          });
+      })
+    );
     if (this.account_id) {
-      this.statisticsService
-        .getMonthlyStatistics(this.account_id)
-        .subscribe((statistics) => {
-          this.monthlyStatistics = statistics;
-          const { total, average } =
-            this.statisticsService.getTotalAndAverageStatistics(statistics);
-          this.total = total;
-          this.average = average;
-        });
-      this.statisticsService
-        .getCategoryStatistics(this.account_id)
-        .subscribe((statistics) => {
-          this.categoryStatistics = statistics;
-        });
+      this.subscription.add(
+        this.statisticsService
+          .getMonthlyStatistics(this.account_id)
+          .subscribe((statistics) => {
+            this.monthlyStatistics = statistics;
+            const { total, average } =
+              this.statisticsService.getTotalAndAverageStatistics(statistics);
+            this.total = total;
+            this.average = average;
+          })
+      );
+      this.subscription.add(
+        this.statisticsService
+          .getCategoryStatistics(this.account_id)
+          .subscribe((statistics) => {
+            this.categoryStatistics = statistics;
+          })
+      );
     }
   }
 }
