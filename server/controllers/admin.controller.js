871188d17b4bbc4533888c36924e63a779d752f9
zFarrukh <diplomatfarruh@gmail.com>
2022-04-22 17:46:36 +0500

    modify admin route to generate statistics to each currency

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -16,7 +16,27 @@ const getCategoryStats = async (req, res) => {
   }
 
   const transactions = await Transaction.find(findQuery);
-  res.json(generateCategoryStats(transactions));
+  const myTransactions = {};
+  for (let i = 0; i < transactions.length; i++) {
+    if (transactions[i].currency) {
+      if (myTransactions[transactions[i].currency]) {
+        myTransactions[transactions[i].currency].push(transactions[i]);
+      } else if (transactions[i].currency) {
+        myTransactions[transactions[i].currency] = [transactions[i]];
+      }
+    }
+  }
+  const categoryStats = {};
+  const currencies = Object.keys(myTransactions);
+  if (currencies.length > 0) {
+    for (let i = 0; i < currencies.length; i++) {
+      categoryStats[currencies[i]] = generateCategoryStats(
+        myTransactions[currencies[i]]
+      );
+    }
+  }
+
+  res.json(categoryStats);
 };
 
 const getMonthlyStats = async (req, res) => {
@@ -31,7 +51,26 @@ const getMonthlyStats = async (req, res) => {
   }
 
   const transactions = await Transaction.find(findQuery);
-  res.json(generateMonthlyStats(transactions));
+  const myTransactions = {};
+  for (let i = 0; i < transactions.length; i++) {
+    if (transactions[i].currency) {
+      if (myTransactions[transactions[i].currency]) {
+        myTransactions[transactions[i].currency].push(transactions[i]);
+      } else {
+        myTransactions[transactions[i].currency] = [transactions[i]];
+      }
+    }
+  }
+  const currencies = Object.keys(myTransactions);
+  const monthlyStats = {};
+  if (currencies.length > 0) {
+    for (let i = 0; i < currencies.length; i++) {
+      monthlyStats[currencies[i]] = generateMonthlyStats(
+        myTransactions[currencies[i]]
+      );
+    }
+  }
+  res.json(monthlyStats);
 };
 
 module.exports = {
